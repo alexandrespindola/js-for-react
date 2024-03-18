a7910494fe8490b2bf99682e79d14097a2ec150b
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-18 16:19:26 +0100

    Adiciona variáveis e função de soma no arquivo index.js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let client = "Juan"
+client = 30
+
+console.log(client)
+
+let price
+
+console.log(price)
+
+console.log(typeof price)
+
+
+let cliente
+let a = 3
+let b = 2
+
+function sum() {
+  a =
+  console.log(a)
+}
+
+console.log(a)
+
+sum()
